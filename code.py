924c16cdc7a9483c2698647d8c12ea8d029e7bfb
manoj2210 <45814826+manoj2210@users.noreply.github.com>
2019-03-19 20:31:20 -0700

    Base for Input Conversion

---
+++ code.py
@@ -0,0 +1,13 @@
+# Python 
+# HARMONIC ANALYSIS
+import copy
+
+
+
+class Inputconversion:
+    
+    def __init__(*Equations):
+        self.Equations=copy.copy(Equations);
+    
+    def Conversionofinput:
+        # Make the text as an latex or equation
