855a9de45021528ab92786bbf60ca23d9ee20cb2
Manoj2210 <manojkumarmaths1000@gmail.com>
2019-10-17 16:17:28 +0530

    internal/Keyboard: Add options for modulus,sqrt,less than,greater than, exponential and constant

--- internal/Keyboard.py
+++ internal/Keyboard.py
@@ -42,6 +42,118 @@ class Ui_Keyboard(object):
 "   }")
         self.Pi_button.setObjectName("Pi_button")
 
+        self.Modulus_button = QtWidgets.QPushButton(Form)
+        self.Modulus_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Modulus_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Modulus_button.setStyleSheet("QPushButton {\n"
+"             background-color: rgba(212, 212, 212, 250);\n"
+"                font: 75 italic 22pt Georgia;\n"
+"                color:rgba(21, 21, 21, 200);\n"
+"                border-radius:30px;\n"
+"             }\n"
+"             QPushButton:hover {\n"
+"            background-color: rgba(212, 212, 212, 52);\n"
+"           color:rgba(21, 21, 21, 250);\n"
+"           border-radius:30px;\n"
+"   }")
+        self.Modulus_button.setObjectName("Modulus_button")
+
+        self.Lessthan_button = QtWidgets.QPushButton(Form)
+        self.Lessthan_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Lessthan_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Lessthan_button.setStyleSheet("QPushButton {\n"
+"             background-color: rgba(212, 212, 212, 250);\n"
+"                font: 75 italic 22pt Georgia;\n"
+"                color:rgba(21, 21, 21, 200);\n"
+"                border-radius:30px;\n"
+"             }\n"
+"             QPushButton:hover {\n"
+"            background-color: rgba(212, 212, 212, 52);\n"
+"           color:rgba(21, 21, 21, 250);\n"
+"           border-radius:30px;\n"
+"   }")
+        self.Lessthan_button.setObjectName("Lessthan_button")
+
+        self.Greaterthan_button = QtWidgets.QPushButton(Form)
+        self.Greaterthan_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Greaterthan_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Greaterthan_button.setStyleSheet("QPushButton {\n"
+"             background-color: rgba(212, 212, 212, 250);\n"
+"                font: 75 italic 22pt Georgia;\n"
+"                color:rgba(21, 21, 21, 200);\n"
+"                border-radius:30px;\n"
+"             }\n"
+"             QPushButton:hover {\n"
+"            background-color: rgba(212, 212, 212, 52);\n"
+"           color:rgba(21, 21, 21, 250);\n"
+"           border-radius:30px;\n"
+"   }")
+        self.Greaterthan_button.setObjectName("Greaterthan_button")
+
+
+        self.Sqrt_button = QtWidgets.QPushButton(Form)
+        self.Sqrt_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Sqrt_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Sqrt_button.setStyleSheet("QPushButton {\n"
+        "             background-color: rgba(212, 212, 212, 250);\n"
+        "                font: 75 italic 22pt Georgia;\n"
+        "                color:rgba(21, 21, 21, 200);\n"
+        "                border-radius:30px;\n"
+        "             }\n"
+        "             QPushButton:hover {\n"
+        "            background-color: rgba(212, 212, 212, 52);\n"
+        "           color:rgba(21, 21, 21, 250);\n"
+        "           border-radius:30px;\n"
+        "   }")
+        self.Sqrt_button.setObjectName("Sqrt_button")
+
+
+
+        self.Exponential_button = QtWidgets.QPushButton(Form)
+        self.Exponential_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Exponential_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Exponential_button.setStyleSheet("QPushButton {\n"
+        "             background-color: rgba(212, 212, 212, 250);\n"
+        "                font: 75 italic 22pt Georgia;\n"
+        "                color:rgba(21, 21, 21, 200);\n"
+        "                border-radius:30px;\n"
+        "             }\n"
+        "             QPushButton:hover {\n"
+        "            background-color: rgba(212, 212, 212, 52);\n"
+        "           color:rgba(21, 21, 21, 250);\n"
+        "           border-radius:30px;\n"
+        "   }")
+        self.Exponential_button.setObjectName("Exponential_button")
+
+
+
+        self.Constant_button = QtWidgets.QPushButton(Form)
+        self.Constant_button.setMinimumSize(QtCore.QSize(120, 61))
+        self.Constant_button.setMaximumSize(QtCore.QSize(140, 61))
+        self.Constant_button.setStyleSheet("QPushButton {\n"
+        "             background-color: rgba(212, 212, 212, 250);\n"
+        "                font: 75 italic 22pt Georgia;\n"
+        "                color:rgba(21, 21, 21, 200);\n"
+        "                border-radius:30px;\n"
+        "             }\n"
+        "             QPushButton:hover {\n"
+        "            background-color: rgba(212, 212, 212, 52);\n"
+        "           color:rgba(21, 21, 21, 250);\n"
+        "           border-radius:30px;\n"
+        "   }")
+        self.Constant_button.setObjectName("Constant_button")
+
+
+
+        self.buttonlayout.addWidget(self.x_pow_n_button)
+        self.buttonlayout.addWidget(self.Pi_button)
+        self.buttonlayout.addWidget(self.Modulus_button)
+        self.buttonlayout.addWidget(self.Lessthan_button)
+        self.buttonlayout.addWidget(self.Greaterthan_button)
+        self.buttonlayout.addWidget(self.Sqrt_button)
+        self.buttonlayout.addWidget(self.Exponential_button)
+        self.buttonlayout.addWidget(self.Constant_button)
+
         self.Base_Layout.addLayout(self.buttonlayout)
 
         self.retranslateUi(Form)
@@ -55,6 +167,12 @@ class Ui_Keyboard(object):
         Form.setWindowTitle(_translate("Form", "Form"))
         self.x_pow_n_button.setText(_translate("Form", "x ⁿ"))
         self.Pi_button.setText(_translate("Form", "π"))
+        self.Modulus_button.setText(_translate("Form", " "))
+        self.Sqrt_button.setText(_translate("Form", "√"))
+        self.Lessthan_button.setText(_translate("Form", "≤"))
+        self.Greaterthan_button.setText(_translate("Form", "≥"))
+        self.Exponential_button.setText(_translate("Form", "℮"))
+        self.Constant_button.setText(_translate("Form", " "))
 
 # if __name__ == "__main__":
 #     import sys
