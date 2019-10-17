654c667bebdd80c34e4a6c4cbcd13d964c29206c
Manoj2210 <manojkumarmaths1000@gmail.com>
2019-10-17 16:26:57 +0530

    setup: update the setup file to work in cross platform environments

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 import os
 
-os.system("pip install PyQt5")
-os.system("pip install numpy")
-os.system("pip install sympy")
-os.system("pip install matplotlib")
-os.system("pip install pyqtgraph")
+print("Using pip or pip3")
+value = "pip"
+value = input()
+if value in ["pip", "pip3"]:
+    print("Installing Packages...")
+else:
+    print("Please enter a valid package installer")
+    exit(0)
+
+command = "{package} install {package_name}"
+
+package_names = ["PyQt5", "numpy", "sympy", "matplotlib", "pyqtgraph", "PyOpenGL", "PyOpenGL_accelerate"]
+
+for x in package_names:
+    print("Installing {x}".format(x=x))
+    os.system(command.format(package=value, package_name=x))
